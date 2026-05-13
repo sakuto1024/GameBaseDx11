a9f588b0080fc00c852e4c33336c6d266336791c
咲音 上関 <250323@jc-21.jp>
2026-05-13 12:14:34 +0900

    おでんの動かし方を少し変えた

--- PlayScene.cpp
+++ PlayScene.cpp
@@ -17,24 +17,39 @@ void PlayScene::Initialize()
 void PlayScene::Update()
 {
 	//ot_.position_ = { 0.0f, 0.0f, 0.0f };
-	
-	ot_.rotate_.y += 0.6f; //回転させる
-	ot_.rotate_.x -= 0.4f;
-	
-	ot_.position_.x += speed_;
-	//ot_.scale_.x += speed_;
-	//ot_.scale_.y += speed_;
-	ot_.scale_.z += speed_;
 
-	if (ot_.position_.x > 2)
-	{
-		speed_ += -0.001f;
-	}
+	static float time = 0.0f;
 
-	if (ot_.position_.x < -2)
-	{
-		speed_ += 0.001f;
-	}
+	ot_.position_ = { 0.0f, 0.0f, 10.0f };
+	ot_.scale_ = { 0.5f, 0.5f, 0.5f };
+	ot_.rotate_ = { 0.0f, 0.0f, 0.0f };
+
+	//ot_.rotate_.y = time;
+	time += 0.025f;
+	ot_.position_.x = 6.0 * sin(time);
+
+	//float posx = 6.0 * sin(0.2f * time);
+	//float posy = 6.0 * cos(3.0f * time);
+	//ot_.position_.x = posx;
+	//ot_.position_.y = posy;
+	
+	//ot_.rotate_.y += 0.6f; //回転させる
+	//ot_.rotate_.x -= 0.4f;
+	//
+	//ot_.position_.x += speed_;
+	////ot_.scale_.x += speed_;
+	////ot_.scale_.y += speed_;
+	//ot_.scale_.z += speed_;
+
+	//if (ot_.position_.x > 2)
+	//{
+	//	speed_ += -0.001f;
+	//}
+
+	//if (ot_.position_.x < -2)
+	//{
+	//	speed_ += 0.001f;
+	//}
 
 }
 
